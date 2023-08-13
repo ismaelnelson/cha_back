c125e167cda9cb73d7dedd1cdd0e8f719d12ce9a
Ismael rosas <129120039+ismaelnelson@users.noreply.github.com>
2023-08-13 11:43:11 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO `chakruk`.`roles` (`id`, `name`)
+INSERT INTO `railway`.`roles` (`id`, `name`)
 SELECT '1', 'ADMIN'
     WHERE NOT EXISTS (SELECT 1 FROM `chakruk`.`roles` WHERE `id` = '1');
 
-INSERT INTO `chakruk`.`roles` (`id`, `name`)
+INSERT INTO `railway`.`roles` (`id`, `name`)
 SELECT '2', 'USER'
     WHERE NOT EXISTS (SELECT 1 FROM `chakruk`.`roles` WHERE `id` = '2');
 
-INSERT INTO `chakruk`.`users` (`id`, `email`, `name`, `nick`,`photo`,`password`,`roles`)
+INSERT INTO `railway`.`users` (`id`, `email`, `name`, `nick`,`photo`,`password`,`roles`)
 SELECT '1','admin@chakruk.com','ADMIN','ADMIN','rey-animal.png','$2a$10$iY0.yv579OFity1Hqy2ybe6YBEsV7QrSOzOl8F1GhKjwjlBTG3nzO','1'
     WHERE NOT EXISTS (SELECT 1 FROM `chakruk`.`users` WHERE `id` = '1');
